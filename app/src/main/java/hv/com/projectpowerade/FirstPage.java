4fe24040c98b8792bd156e0b4a138e0247577f75
marchesed <marchesedb@gmail.com>
2017-01-07 14:05:54 -0500

    initialize repository

--- app/src/main/java/hv/com/projectpowerade/FirstPage.java
+++ app/src/main/java/hv/com/projectpowerade/FirstPage.java
@@ -9,6 +9,8 @@ public class FirstPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_page);
+        //jwbefkjwqef
+
     }
 }
 
