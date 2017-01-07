874903d8205c3a1b4a2438bf931d7f2b8c8ed899
marchesed <marchesedb@gmail.com>
2017-01-07 14:04:16 -0500

    initialize repository

---
+++ app/src/main/java/hv/com/projectpowerade/FirstPage.java
@@ -0,0 +1,14 @@
+package hv.com.projectpowerade;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_page);
+    }
+}
+
