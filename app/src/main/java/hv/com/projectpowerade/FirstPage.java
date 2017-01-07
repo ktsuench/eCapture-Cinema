5f124233a683d62dbc075a7656cd0eb5b2cf99c5
marchesed <marchesedb@gmail.com>
2017-01-07 14:51:39 -0500

    initialize repository

--- app/src/main/java/hv/com/projectpowerade/FirstPage.java
+++ app/src/main/java/hv/com/projectpowerade/FirstPage.java
@@ -1,16 +1,27 @@
 package hv.com.projectpowerade;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FirstPage extends AppCompatActivity {
-
+    private Button start;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_page);
-        //jwbefkjwqef
 
+        start = (Button)findViewById(R.id.button1);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotoPoster = new Intent(FirstPage.this, PosterPage.class);
+                startActivity(gotoPoster);
+            }
+        });
     }
 }
 
---
+++ app/src/main/java/hv/com/projectpowerade/PosterPage.java
@@ -0,0 +1,18 @@
+package hv.com.projectpowerade;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by domenic on 2017-01-07.
+ */
+
+public class PosterPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.poster_page);
+    }
+
+}
