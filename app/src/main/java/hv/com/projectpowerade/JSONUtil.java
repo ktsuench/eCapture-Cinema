4b6284ad05a393840c6518beec29d90a338a2f85
ktsuench <will289@hotmail.com>
2017-01-08 09:01:23 -0500

    added JSONUtils

---
+++ app/src/main/java/hv/com/projectpowerade/JSONUtil.java
@@ -0,0 +1,44 @@
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by brentmarks on 2017-01-08.
+ */
+
+public class JSONUtil {
+
+    public static String toJSon(ArrayList<Genre> genres) {
+        try {
+            JSONArray jsonArray = new JSONArray();
+            if(genres != null) {
+                for (Genre g : genres) {
+                    JSONObject genre = new JSONObject();
+                    genre.put("name", g.getName());
+                    genre.put("id", g.getId());
+                    genre.put("fileName", g.getFileName());
+                    if(g.getSubCategories() != null){
+                        JSONArray subCategories = new JSONArray();
+                        for(Genre subG : g.getSubCategories()){
+                            JSONObject genreSub = new JSONObject();
+                            genre.put("name", subG.getName());
+                            genre.put("id", subG.getId());
+                            genre.put("fileName", subG.getFileName());
+                            subCategories.put(genreSub);
+                        }
+                        genre.put("subCategories", subCategories);
+                    }
+                }
+            }
+
+            return jsonArray.toString();
+
+        } catch (JSONException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+
+    }
+}
